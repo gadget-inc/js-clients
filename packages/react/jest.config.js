bdef675fd248bc131db4b725bc9b32968d8639d4
Harry Brundage <harry.brundage@gmail.com>
2021-10-06 12:01:38 -0400

    Get jest working

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: process.env.LAYERCI ? "/tmp/jest-cache" : "<rootDir>/../../tmp/cache/jest",
+  // cacheDirectory: process.env.LAYERCI ? "/tmp/jest-cache" : undefined,
 
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
