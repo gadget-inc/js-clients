2df0b1d8212606be8577e28f92463c721bb93940
Jason Gedge <jason@gadget.dev>
2021-10-27 14:14:14 -0400

    Remove `currentSession` from `AnyClient`
    There's no guarantee that there will be such a manager, nor can we guarantee the
    actions `logInViaEmail` or `logOut` will be there even if it is.

--- packages/api-client-core/src/AnyClient.ts
+++ packages/api-client-core/src/AnyClient.ts
@@ -6,11 +6,6 @@ export interface AnyClient {
   connection: GadgetConnection;
   query(graphQL: string, variables?: Record<string, any>): Promise<any>;
   mutate(graphQL: string, variables?: Record<string, any>): Promise<any>;
-  currentSession: {
-    get(): Promise<any>;
-    logInViaEmail?: (input: any) => Promise<any>;
-    logOut?: () => Promise<any>;
-  };
   transaction<T>(callback: (transaction: GadgetTransaction) => Promise<T>): Promise<T>;
   internal: {
     [key: string]: InternalModelManager;
