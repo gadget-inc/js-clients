14f8416d43f0a2bdec8f9b50d96d1e056ccb2577
Harry Brundage <harry.brundage@gmail.com>
2021-11-10 22:56:10 +0000

    Source two example clients in the workspace to use when testing

---
+++ packages/react/spec/apis.ts
@@ -0,0 +1,5 @@
+import { Client as BulkClient } from "@gadget-client/bulk-actions-example";
+import { Client } from "@gadget-client/related-products-example";
+
+export const relatedProductsApi = new Client();
+export const bulkExampleApi = new BulkClient();
