6e713134370f8b213b371f8b25bd100ee9679224
kristianpd <kristianpd@gmail.com>
2022-06-02 11:28:45 -0400

    Add basic useGadget type assertion test

---
+++ packages/react-shopify-app-bridge/spec/useGadget.spec.ts
@@ -0,0 +1,17 @@
+import { AppBridgeState, ClientApplication } from "@shopify/app-bridge";
+import { assert, IsExact } from "conditional-type-checks";
+import { AppType, useGadget } from "../src";
+
+// these functions are typechecked but never run to avoid actually making API calls
+const TestUseGadgetReturnsAppropriateTypes = () => {
+  const { loading, appType, isEmbedded, appBridge, canAuth, isAuthenticated } = useGadget();
+
+  assert<IsExact<typeof loading, boolean>>(true);
+  assert<IsExact<typeof isEmbedded, boolean>>(true);
+  assert<IsExact<typeof canAuth, boolean>>(true);
+  assert<IsExact<typeof isAuthenticated, boolean>>(true);
+  assert<IsExact<typeof appType, AppType | undefined>>(true);
+  assert<IsExact<typeof appBridge, ClientApplication<AppBridgeState> | undefined>>(true);
+};
+
+test("true", () => undefined);
