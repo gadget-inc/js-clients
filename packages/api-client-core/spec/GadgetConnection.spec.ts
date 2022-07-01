822cc7df7b0e4dda6860b256772a978cffe2a841
Harry Brundage <harry.brundage@gmail.com>
2022-07-01 11:36:13 -0400

    Expand test coverage of the GadgetConnection auth modes

--- packages/api-client-core/spec/GadgetConnection.spec.ts
+++ packages/api-client-core/spec/GadgetConnection.spec.ts
@@ -1,7 +1,20 @@
+import gql from "gql-tag";
+import nock from "nock";
+import { BrowserSessionStorageType } from "../src";
 import { AuthenticationMode, GadgetConnection } from "../src/GadgetConnection";
 
+const base64 = (str: string) => Buffer.from(str).toString("base64");
+nock.disableNetConnect();
+
 describe("GadgetConnection", () => {
-  // this object is integration tested within Gadget itself
+  // this object is integration tested within Gadget itself also
+  beforeEach(() => {
+    nock.cleanAll();
+  });
+
+  afterEach(() => {
+    expect(nock.pendingMocks()).toEqual([]);
+  });
 
   it("should require an endpoint to be set", () => {
     expect(() => new GadgetConnection({} as any)).toThrowErrorMatchingInlineSnapshot(
@@ -10,7 +23,261 @@ describe("GadgetConnection", () => {
   });
 
   it("should default to the anonymous authentication mode if no authentication mode is passed", () => {
-    const connection = new GadgetConnection({ endpoint: "http://someapp.gadget.host" });
+    const connection = new GadgetConnection({ endpoint: "https://someapp.gadget.app" });
     expect(connection.authenticationMode).toEqual(AuthenticationMode.Anonymous);
+  });
+
+  describe("authorization", () => {
+    it("should allow connecting with anonymous authentication", async () => {
+      nock("https://someapp.gadget.app")
+        .post("/api/graphql", { query: "{\n  meta {\n    appName\n  }\n}\n", variables: {} })
+        .reply(200, {
+          data: {
+            meta: {
+              appName: "some app",
+            },
+          },
+        });
+
+      const connection = new GadgetConnection({
+        endpoint: "https://someapp.gadget.app/api/graphql",
+        authenticationMode: { anonymous: true },
+      });
+
+      const result = await connection.currentClient
+        .query(
+          gql`
+            {
+              meta {
+                appName
+              }
+            }
+          `
+        )
+        .toPromise();
+
+      expect(result.data).toEqual({ meta: { appName: "some app" } });
+    });
+
+    it("should allow connecting with internal auth token authentication", async () => {
+      nock("https://someapp.gadget.app")
+        .post("/api/graphql", { query: "{\n  meta {\n    appName\n  }\n}\n", variables: {} })
+        // .basicAuth({ user: "gadget", pass: "opaque-token-thing" })
+        .reply(200, function () {
+          expect(this.req.headers["authorization"]).toEqual([`Basic ${base64("gadget-internal:opaque-token-thing")}`]);
+
+          return {
+            data: {
+              meta: {
+                appName: "some app",
+              },
+            },
+          };
+        });
+
+      const connection = new GadgetConnection({
+        endpoint: "https://someapp.gadget.app/api/graphql",
+        authenticationMode: { internalAuthToken: "opaque-token-thing" },
+      });
+
+      const result = await connection.currentClient
+        .query(
+          gql`
+            {
+              meta {
+                appName
+              }
+            }
+          `
+        )
+        .toPromise();
+
+      expect(result.data).toEqual({ meta: { appName: "some app" } });
+    });
+
+    it("should allow connecting with a gadget API Key", async () => {
+      nock("https://someapp.gadget.app")
+        .post("/api/graphql", { query: "{\n  meta {\n    appName\n  }\n}\n", variables: {} })
+        .reply(200, function () {
+          expect(this.req.headers["authorization"]).toEqual([`Bearer gsk-abcde`]);
+
+          return {
+            data: {
+              meta: {
+                appName: "some app",
+              },
+            },
+          };
+        });
+
+      const connection = new GadgetConnection({
+        endpoint: "https://someapp.gadget.app/api/graphql",
+        authenticationMode: { apiKey: "gsk-abcde" },
+      });
+
+      const result = await connection.currentClient
+        .query(
+          gql`
+            {
+              meta {
+                appName
+              }
+            }
+          `
+        )
+        .toPromise();
+
+      expect(result.data).toEqual({ meta: { appName: "some app" } });
+    });
+
+    describe("session token storage", () => {
+      it("should allow connecting with no session in a session storage mode", async () => {
+        nock("https://someapp.gadget.app")
+          .post("/api/graphql", { query: "{\n  meta {\n    appName\n  }\n}\n", variables: {} })
+          .reply(
+            200,
+            {
+              data: {
+                meta: {
+                  appName: "some app",
+                },
+              },
+            },
+            {
+              "x-set-authorization": "Session token-123",
+            }
+          );
+
+        const connection = new GadgetConnection({
+          endpoint: "https://someapp.gadget.app/api/graphql",
+          authenticationMode: { browserSession: { storageType: BrowserSessionStorageType.Temporary } },
+        });
+
+        const result = await connection.currentClient
+          .query(
+            gql`
+              {
+                meta {
+                  appName
+                }
+              }
+            `
+          )
+          .toPromise();
+
+        expect(result.data).toEqual({ meta: { appName: "some app" } });
+      });
+
+      it("should allow connecting with an initial session token in session storage mode", async () => {
+        nock("https://someapp.gadget.app")
+          .post("/api/graphql", { query: "{\n  meta {\n    appName\n  }\n}\n", variables: {} })
+          .reply(
+            200,
+            function () {
+              expect(this.req.headers["authorization"]).toEqual(["Session token-123"]);
+              return {
+                data: {
+                  meta: {
+                    appName: "some app",
+                  },
+                },
+              };
+            },
+            {
+              "x-set-authorization": "Session token-123",
+            }
+          );
+
+        const connection = new GadgetConnection({
+          endpoint: "https://someapp.gadget.app/api/graphql",
+          authenticationMode: { browserSession: { storageType: BrowserSessionStorageType.Temporary, initialToken: "token-123" } },
+        });
+
+        const result = await connection.currentClient
+          .query(
+            gql`
+              {
+                meta {
+                  appName
+                }
+              }
+            `
+          )
+          .toPromise();
+
+        expect(result.data).toEqual({ meta: { appName: "some app" } });
+      });
+
+      it("should store a x-set-authorization header and reuse it for subsequent requests", async () => {
+        nock("https://someapp.gadget.app")
+          .post("/api/graphql", { query: "{\n  meta {\n    appName\n  }\n}\n", variables: {} })
+          .reply(
+            200,
+            function () {
+              expect(this.req.headers["authorization"]).toBeUndefined();
+              return {
+                data: {
+                  meta: {
+                    appName: "some app",
+                  },
+                },
+              };
+            },
+            {
+              "x-set-authorization": "Session token-123",
+            }
+          )
+          .post("/api/graphql", { query: "{\n  currentSession {\n    id\n  }\n}\n", variables: {} })
+          .reply(
+            200,
+            function () {
+              expect(this.req.headers["authorization"]).toEqual(["Session token-123"]);
+              return {
+                data: {
+                  currentSession: {
+                    id: 1,
+                  },
+                },
+              };
+            },
+            {
+              "x-set-authorization": "Session token-123",
+            }
+          );
+
+        const connection = new GadgetConnection({
+          endpoint: "https://someapp.gadget.app/api/graphql",
+          authenticationMode: { browserSession: { storageType: BrowserSessionStorageType.Temporary } },
+        });
+
+        const firstResult = await connection.currentClient
+          .query(
+            gql`
+              {
+                meta {
+                  appName
+                }
+              }
+            `
+          )
+          .toPromise();
+
+        expect(firstResult.data).toEqual({ meta: { appName: "some app" } });
+
+        const secondResult = await connection.currentClient
+          .query(
+            gql`
+              {
+                currentSession {
+                  id
+                }
+              }
+            `
+          )
+          .toPromise();
+
+        expect(secondResult.data).toEqual({ currentSession: { id: 1 } });
+      });
+    });
   });
 });
