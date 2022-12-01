6b623b5219622bae739ee83770f2a4100b419c2b
Scott Côté <scott.cote@hey.com>
2022-12-01 18:23:11 -0500

    gitpkg

---
+++ dist/src/DataHydrator.d.ts
@@ -0,0 +1,20 @@
+export declare const Hydrators: {
+    DateTime(value: string): Date;
+};
+export declare type Hydration = keyof typeof Hydrators;
+export interface HydrationPlan {
+    [key: string]: Hydration;
+}
+/**
+ * Utility for declaratively transforming object trees.
+ * Useful for turning API date strings into real Date objects, etc.
+ * Declarative so that the operations it peforms can be serialized.
+ */
+export declare class DataHydrator {
+    readonly plan: HydrationPlan;
+    constructor(plan: HydrationPlan);
+    apply(source: Record<string, any> | Record<string, any>[]): {
+        [x: string]: any;
+    };
+    private hydrateObject;
+}
