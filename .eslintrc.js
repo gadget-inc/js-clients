0234f8179f2ef982710e6a52a7fa46c43dd080b6
Harry Brundage <harry.brundage@gmail.com>
2023-04-19 10:19:40 -0400

    Ensure eslint typescript can find the tsconfig for the vite.config.ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: "@gadgetinc/eslint-config",
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ["./packages/*/tsconfig.json", "./scripts/tsconfig.json"],
+    project: ["./packages/*/tsconfig.json", "./scripts/tsconfig.json", "./packages/blog-example/tsconfig.node.json"],
   },
   rules: {
     "lodash/import-scope": "off",
