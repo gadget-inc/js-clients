059884ad2d905ff9805a0b4fd89352e3271d1003
Harry Brundage <harry.brundage@gmail.com>
2023-06-28 22:41:37 -0400

    gitpkg

---
+++ dist/src/useGadgetQuery.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useGadgetQuery = void 0;
+const react_1 = require("react");
+const urql_1 = require("urql");
+const GadgetProvider_1 = require("./GadgetProvider");
+const utils_1 = require("./utils");
+const useGadgetQuery = (args) => {
+    if (!(0, react_1.useContext)(GadgetProvider_1.GadgetUrqlClientContext))
+        throw new Error(utils_1.noProviderErrorMessage);
+    return (0, urql_1.useQuery)(args);
+};
+exports.useGadgetQuery = useGadgetQuery;
+//# sourceMappingURL=useGadgetQuery.js.map
