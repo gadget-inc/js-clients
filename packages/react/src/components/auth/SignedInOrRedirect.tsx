180b80f8b34917f1229f99e9275ea8034f8bc574
jcao49 <jenny@gadget.dev>
2023-07-14 12:35:55 -0400

    update SignedInOrRedirect to use useAuth

--- packages/react/src/components/auth/SignedInOrRedirect.tsx
+++ packages/react/src/components/auth/SignedInOrRedirect.tsx
@@ -1,27 +1,26 @@
 import type { ReactNode } from "react";
 import React, { useContext, useEffect, useState } from "react";
 import { GadgetConfigurationContext } from "../../../src/GadgetProvider";
-import { useSession } from "../../../src/auth/useSession";
-import { isSessionSignedIn } from "../../../src/auth/utils";
+import { useAuth } from "../../auth/useAuth";
 
 /**
  * Renders its `children` if the current `Session` is signed in, otherwise redirects the browser to the `signInPath` configured in the `Provider`. Uses `window.location.assign` to perform the redirect.
  */
 export const SignedInOrRedirect = (props: { children: ReactNode }) => {
   const [redirected, setRedirected] = useState(false);
-  const session = useSession();
-  const isSignedIn = session && isSessionSignedIn(session);
+
+  const { user, isSignedIn } = useAuth();
   const context = useContext(GadgetConfigurationContext);
   const { auth } = context ?? {};
 
   useEffect(() => {
-    if (auth && !redirected && !isSignedIn) {
+    if (auth && !redirected && !isSignedIn ) {
       setRedirected(true);
       window.location.assign(auth.signInPath);
     }
   }, [redirected, isSignedIn, auth]);
 
-  if (isSignedIn) {
+  if (user && isSignedIn) {
     return <>{props.children}</>;
   } else {
     return null;
