4a36317e08c07d1018846a929e1ae875a6add90d
jcao49 <jenny@gadget.dev>
2023-07-24 15:54:23 -0400

    Add SignedOutOrRedirect

---
+++ packages/react/src/components/auth/SignedOutOrRedirect.tsx
@@ -0,0 +1,30 @@
+import type { ReactNode } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { GadgetConfigurationContext } from "../../../src/GadgetProvider";
+import { useAuth } from "../../auth/useAuth";
+
+/**
+ * Renders its `children` if the current `Session` is signed out, otherwise redirects the browser to the `signOutPath` configured in the `Provider`. Uses `window.location.assign` to perform the redirect.
+ */
+export const SignedOutOrRedirect = (props: { children: ReactNode }) => {
+  const [redirected, setRedirected] = useState(false);
+
+  const { user, isSignedIn } = useAuth();
+  const context = useContext(GadgetConfigurationContext);
+  const { auth } = context ?? {};
+
+  useEffect(() => {
+    if (auth && !redirected && (isSignedIn || user)) {
+        setRedirected(true);
+        const redirectUrl = new URL(auth.signInPath, window.location.origin);
+        redirectUrl.searchParams.set("redirectTo", window.location.pathname);
+        window.location.assign(redirectUrl.toString());
+    }
+  }, [redirected, isSignedIn, auth]);
+
+  if (!user && !isSignedIn) {
+    return <>{props.children}</>;
+  } else {
+    return null;
+  }
+};
