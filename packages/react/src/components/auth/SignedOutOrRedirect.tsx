dd06ab2d918c9012655d4434fb1daa3e1f6cd0c4
Kristian PD <kristianpd@gmail.com>
2023-07-25 15:40:12 -0400

    Use path prop in favor of provider config for SignedOutOrRedirect

--- packages/react/src/components/auth/SignedOutOrRedirect.tsx
+++ packages/react/src/components/auth/SignedOutOrRedirect.tsx
@@ -4,10 +4,11 @@ import { GadgetConfigurationContext } from "../../../src/GadgetProvider";
 import { useAuth } from "../../auth/useAuth";
 
 /**
- * Renders its `children` if the current `Session` is signed out, otherwise redirects the browser to the `signOutPath` configured in the `Provider`. Uses `window.location.assign` to perform the redirect.
+ * Renders its `children` if the current `Session` is signed out, otherwise redirects the browser to the `path` prop. Uses `window.location.assign` to perform the redirect.
  */
-export const SignedOutOrRedirect = (props: { children: ReactNode }) => {
+export const SignedOutOrRedirect = (props: { path: string, children: ReactNode }) => {
   const [redirected, setRedirected] = useState(false);
+  const { path, children } = props;
 
   const { user, isSignedIn } = useAuth();
   const context = useContext(GadgetConfigurationContext);
@@ -16,14 +17,13 @@ export const SignedOutOrRedirect = (props: { children: ReactNode }) => {
   useEffect(() => {
     if (auth && !redirected && (isSignedIn || user)) {
       setRedirected(true);
-      const redirectUrl = new URL(auth.signOutPath, window.location.origin);
-      redirectUrl.searchParams.set("redirectTo", window.location.pathname);
+      const redirectUrl = new URL(path, window.location.origin);
       window.location.assign(redirectUrl.toString());
     }
-  }, [redirected, isSignedIn, auth]);
+  }, [redirected, isSignedIn, auth, path, user]);
 
   if (!user && !isSignedIn) {
-    return <>{props.children}</>;
+    return <>{children}</>;
   } else {
     return null;
   }
