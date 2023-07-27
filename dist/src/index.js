4de31e93da532a3ff0aae6c75842b18b2cd1741a
Kristian PD <kristianpd@gmail.com>
2023-07-27 12:10:35 -0400

    gitpkg

---
+++ dist/src/index.js
@@ -0,0 +1,33 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./AnyClient"), exports);
+__exportStar(require("./ClientOptions"), exports);
+__exportStar(require("./DataHydrator"), exports);
+__exportStar(require("./FieldSelection"), exports);
+__exportStar(require("./GadgetConnection"), exports);
+__exportStar(require("./GadgetFunctions"), exports);
+__exportStar(require("./GadgetRecord"), exports);
+__exportStar(require("./GadgetRecordList"), exports);
+__exportStar(require("./GadgetTransaction"), exports);
+__exportStar(require("./InMemoryStorage"), exports);
+__exportStar(require("./InternalModelManager"), exports);
+__exportStar(require("./ModelManager"), exports);
+__exportStar(require("./operationBuilders"), exports);
+__exportStar(require("./operationRunners"), exports);
+__exportStar(require("./support"), exports);
+__exportStar(require("./types"), exports);
+//# sourceMappingURL=index.js.map
