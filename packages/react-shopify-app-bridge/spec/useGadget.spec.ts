142facbf1b37dc86716e5346141288e2ec83bbff
Harry Brundage <harry.brundage@gmail.com>
2023-08-04 14:37:58 -0400

    Convert react-shopify-app-bridge to also be CJS/ESM
    This fixes a bug where react-shopify-app-bridge being CJS only breaks vite setups that use the ESM/CJS versions of @gadgetinc/react as well, where vite imported the ESM version of the react context and the CJS version in two different spots, resulting in context identities not matching.

--- packages/react-shopify-app-bridge/spec/useGadget.spec.ts
+++ packages/react-shopify-app-bridge/spec/useGadget.spec.ts
@@ -1,8 +1,8 @@
 import type { AppBridgeState, ClientApplication } from "@shopify/app-bridge";
 import type { IsExact } from "conditional-type-checks";
 import { assert } from "conditional-type-checks";
-import type { AppType } from "../src";
-import { useGadget } from "../src";
+import type { AppType } from "../src/index.js";
+import { useGadget } from "../src/index.js";
 
 // these functions are typechecked but never run to avoid actually making API calls
 const TestUseGadgetReturnsAppropriateTypes = () => {
--- packages/react-shopify-app-bridge/src/index.ts
+++ packages/react-shopify-app-bridge/src/index.ts
@@ -1,2 +1,2 @@
-export * from "./Provider";
-export * from "./context";
+export * from "./Provider.js";
+export * from "./context.js";
