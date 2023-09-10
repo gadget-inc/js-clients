d8f647d36c314fb0822a7b2d0702619c726839c2
Harry Brundage <harry.brundage@gmail.com>
2023-09-10 20:52:51 +0000

    Slim down the size of the isEqual helper by importing a simpler one
    We were previously using isEqual from lodash for simplicity in a structural object comparison, but it is a remarkably big package at 50kb uncompressed. This pulls in a smaller version of a very similar utility without any dependencies to replace it.
    
    Before:
    
    ```
    ✓ 176 modules transformed.
    dist/api-read-stats.html      212.12 kB │ gzip: 47.04 kB
    dist/test-bundle-api-read.js  236.85 kB │ gzip: 43.50 kB
    dist/api-read-stats.html       212.64 kB │ gzip: 47.09 kB
    dist/test-bundle-api-read.cjs  155.96 kB │ gzip: 36.01 kB
    ✓ built in 861ms
    vite v4.4.7 building for production...
    ✓ 452 modules transformed.
    dist/react-read-stats.html      303.58 kB │ gzip: 57.00 kB
    dist/test-bundle-react-read.js  342.04 kB │ gzip: 72.64 kB
    dist/react-read-stats.html       307.55 kB │ gzip: 57.01 kB
    dist/test-bundle-react-read.cjs  230.74 kB │ gzip: 61.17 kB
    ✓ built in 1.23s
    vite v4.4.7 building for production...
    ✓ 986 modules transformed.
    dist/shopify-read-stats.html      517.13 kB │ gzip:  77.52 kB
    dist/test-bundle-shopify-read.js  661.41 kB │ gzip: 131.38 kB
    dist/shopify-read-stats.html       517.64 kB │ gzip:  77.55 kB
    dist/test-bundle-shopify-read.cjs  464.83 kB │ gzip: 112.29 kB
    ✓ built in 2.39s
    ```
    
    After:
    
    ```
    vite v4.4.7 building for production...
    ✓ 174 modules transformed.
    dist/api-read-stats.html      211.29 kB │ gzip: 46.97 kB
    dist/test-bundle-api-read.js  223.49 kB │ gzip: 40.77 kB
    dist/api-read-stats.html       211.85 kB │ gzip: 46.99 kB
    dist/test-bundle-api-read.cjs  146.84 kB │ gzip: 33.65 kB
    ✓ built in 795ms
    vite v4.4.7 building for production...
    ✓ 450 modules transformed.
    dist/react-read-stats.html      302.75 kB │ gzip: 56.82 kB
    dist/test-bundle-react-read.js  328.63 kB │ gzip: 69.94 kB
    dist/react-read-stats.html       306.74 kB │ gzip: 56.90 kB
    dist/test-bundle-react-read.cjs  221.58 kB │ gzip: 58.80 kB
    ✓ built in 1.21s
    vite v4.4.7 building for production...
    ✓ 984 modules transformed.
    dist/shopify-read-stats.html      516.32 kB │ gzip:  77.44 kB
    dist/test-bundle-shopify-read.js  647.97 kB │ gzip: 128.67 kB
    dist/shopify-read-stats.html       516.83 kB │ gzip:  77.47 kB
    dist/test-bundle-shopify-read.cjs  455.63 kB │ gzip: 109.84 kB
    ✓ built in 2.60s
    ```
    
    6% bundle size reduction

--- packages/api-client-core/src/support.ts
+++ packages/api-client-core/src/support.ts
@@ -436,3 +436,97 @@ export const storageAvailable = (type: "localStorage" | "sessionStorage") => {
     return false;
   }
 };
+
+// smaller implementation of lodash's isEqual from https://github.com/NickGard/tiny-isequal but made a bit more performant and typesafe
+const toString = Object.prototype.toString,
+  getPrototypeOf = Object.getPrototypeOf,
+  getOwnProperties = Object.getOwnPropertySymbols
+    ? (c: any) => (Object.keys(c) as any[]).concat(Object.getOwnPropertySymbols(c))
+    : Object.keys;
+
+const checkEquality = (a: any, b: any, refs: any[]): boolean => {
+  // trivial case: primitives and referentially equal objects
+  if (a === b) return true;
+
+  // if both are null/undefined, the above check would have returned true
+  if (a == null || b == null) return false;
+
+  // check to see if we've seen this reference before; if yes, return true
+  // eslint-disable-next-line lodash/prefer-includes
+  if (refs.indexOf(a) > -1 && refs.indexOf(b) > -1) return true;
+
+  const aType = toString.call(a);
+  const bType = toString.call(b);
+
+  let aElements, bElements, element;
+
+  // save results for circular checks
+  refs.push(a, b);
+
+  if (aType != bType) return false; // not the same type of objects
+
+  // for non-null objects, check all custom properties
+  aElements = getOwnProperties(a);
+  bElements = getOwnProperties(b);
+  if (
+    aElements.length != bElements.length ||
+    aElements.some(function (key) {
+      return !checkEquality(a[key], b[key], refs);
+    })
+  ) {
+    return false;
+  }
+
+  switch (aType.slice(8, -1)) {
+    case "Symbol":
+      return a.valueOf() == b.valueOf();
+    case "Date":
+    case "Number":
+      return +a == +b || (+a != +a && +b != +b); // convert Dates to ms, check for NaN
+    case "RegExp":
+    case "Function":
+    case "String":
+    case "Boolean":
+      return "" + a == "" + b;
+    case "Set":
+    case "Map": {
+      aElements = a.entries();
+      bElements = b.entries();
+      do {
+        element = aElements.next();
+        if (!checkEquality(element.value, bElements.next().value, refs)) {
+          return false;
+        }
+      } while (!element.done);
+      return true;
+    }
+    case "ArrayBuffer":
+      (a = new Uint8Array(a)), (b = new Uint8Array(b)); // fall through to be handled as an Array
+    case "DataView":
+      (a = new Uint8Array(a.buffer)), (b = new Uint8Array(b.buffer)); // fall through to be handled as an Array
+    case "Float32Array":
+    case "Float64Array":
+    case "Int8Array":
+    case "Int16Array":
+    case "Int32Array":
+    case "Uint8Array":
+    case "Uint16Array":
+    case "Uint32Array":
+    case "Uint8ClampedArray":
+    case "Arguments":
+    case "Array":
+      if (a.length != b.length) return false;
+      for (element = 0; element < a.length; element++) {
+        if (!(element in a) && !(element in b)) continue; // empty slots are equal
+        // either one slot is empty but not both OR the elements are not equal
+        if (element in a != element in b || !checkEquality(a[element], b[element], refs)) return false;
+      }
+      return true;
+    case "Object":
+      return checkEquality(getPrototypeOf(a), getPrototypeOf(b), refs);
+    default:
+      return false;
+  }
+};
+
+export const isEqual = (a: any, b: any) => checkEquality(a, b, []);
