371d68443bc868946c87dc2ebcdbf64615a9dc7a
Harry Brundage <harry.brundage@gmail.com>
2023-10-01 18:50:31 -0400

    Suppress invalid test environment warnings by explicitly targetting development for test api clients

--- packages/react/spec/apis.ts
+++ packages/react/spec/apis.ts
@@ -3,7 +3,7 @@ import { Client as BulkClient } from "@gadget-client/bulk-actions-test";
 import { Client } from "@gadget-client/related-products-example";
 import { Client as AuthClient } from "@gadget-client/super-auth";
 
-export const relatedProductsApi = new Client();
-export const bulkExampleApi = new BulkClient();
-export const superAuthApi = new AuthClient();
-export const noUserModelApi = new NoUserClient();
+export const relatedProductsApi = new Client({ environment: "Development" });
+export const bulkExampleApi = new BulkClient({ environment: "Development" });
+export const superAuthApi = new AuthClient({ environment: "Development" });
+export const noUserModelApi = new NoUserClient({ environment: "Development" });
