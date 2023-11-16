15b7915bf9c3d1a235b52aff4eeafc8b146d531a
Harry Brundage <harry.brundage@gmail.com>
2023-11-16 15:45:32 +0000

    Add support for endpoints with existing query params
    This is useful for gadget internally passing endpoints with existing query params to use for tagging

--- packages/api-client-core/src/exchanges/urlParamExchange.ts
+++ packages/api-client-core/src/exchanges/urlParamExchange.ts
@@ -2,8 +2,15 @@ import { mapExchange } from "@urql/core";
 
 export const urlParamExchange = mapExchange({
   onOperation: (operation) => {
-    if (operation.context.url && operation.context.operationName && !operation.context.url.includes("?")) {
-      operation.context.url += `?operation=${operation.context.operationName}`;
+    if (operation.context.url && operation.context.operationName) {
+      try {
+        const [start, params] = operation.context.url.split("?");
+        const paramsObj = new URLSearchParams(params);
+        paramsObj.set("operation", operation.context.operationName);
+        operation.context.url = `${start}?${paramsObj.toString()}`;
+      } catch (error) {
+        // not able to parse URL params, just don't add this optional param and let the rest of the system react to the invalid URL
+      }
     }
   },
 });
