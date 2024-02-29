ec6ebdc8704fa74f6dfe4bb9674ca67556d6ca05
Maneet Goyal <maneetgoyal14@gmail.com>
2024-02-29 13:16:39 +0530

    refactor: remove @n1ru4l/graphql-live-query-patch-jsondiffpatch

--- packages/api-client-core/src/graphql-live-query-utils/index.ts
+++ packages/api-client-core/src/graphql-live-query-utils/index.ts
@@ -1,4 +1,18 @@
-import { applyJSONDiffPatch } from "@n1ru4l/graphql-live-query-patch-jsondiffpatch";
+import { patch, Delta } from "@n1ru4l/json-patch-plus";
 import { createApplyLiveQueryPatch } from "./createApplyLiveQueryPatch.js";
+
+export type ApplyPatchFunction<PatchPayload = unknown> = (
+    previous: Record<string, unknown>,
+    patch: PatchPayload
+) => Record<string, unknown>;
+
+export const applyJSONDiffPatch: ApplyPatchFunction<Delta> = (
+    left,
+    delta
+): Record<string, unknown> =>
+    patch({
+        left,
+        delta,
+    });
 
 export const applyLiveQueryJSONDiffPatch = createApplyLiveQueryPatch(applyJSONDiffPatch);
