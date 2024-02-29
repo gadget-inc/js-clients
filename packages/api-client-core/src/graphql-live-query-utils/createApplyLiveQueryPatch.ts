efda499cb4bec7959e33923e6cdb37ccd9a93187
Maneet Goyal <maneetgoyal14@gmail.com>
2024-02-29 12:48:23 +0530

    refactor: Add createApplyLiveQueryPatch

---
+++ packages/api-client-core/src/graphql-live-query-utils/createApplyLiveQueryPatch.ts
@@ -0,0 +1,70 @@
+import { Repeater } from "@manufac/repeater";
+import type { ExecutionLivePatchResult } from "@n1ru4l/graphql-live-query-patch";
+import type { ExecutionResult } from "graphql";
+
+export type ApplyPatchFunction<PatchPayload = unknown> = (
+  previous: Record<string, unknown>,
+  patch: PatchPayload
+) => Record<string, unknown>;
+
+/**
+ * Create a middleware generator function for applying live query patches on the client.
+ */
+export const createApplyLiveQueryPatch =
+  <PatchPayload = unknown>(
+    /* Function which is used for generating the patches */
+    applyPatch: ApplyPatchFunction<PatchPayload>
+  ) =>
+  <TExecutionResult = Record<string, unknown>>(source: AsyncIterable<TExecutionResult>) =>
+    new Repeater<TExecutionResult>(async (push, stop) => {
+      const iterator = source[Symbol.asyncIterator]();
+      stop.then(() => iterator.return?.()).catch(console.log);
+      let mutableData: ExecutionResult | null = null;
+      let lastRevision = 0;
+      let next: IteratorResult<ExecutionLivePatchResult<PatchPayload>>;
+      // @ts-expect-error Adopted from https://github.com/n1ru4l/graphql-live-query/blob/main/packages/graphql-live-query-patch/src/createApplyLiveQueryPatch.ts
+      while ((next = await iterator.next()).done === false) {
+        // no revision means this is no live query patch.
+        if ("revision" in next.value && next.value.revision) {
+          const valueToPublish: ExecutionLivePatchResult = {};
+
+          if (next.value.revision === 1) {
+            if (!next.value.data) {
+              throw new Error("Missing data.");
+            }
+            valueToPublish.data = next.value.data;
+            mutableData = next.value.data;
+            lastRevision = 1;
+          } else {
+            if (!mutableData) {
+              throw new Error("No previousData available.");
+            }
+            if (!next.value.patch) {
+              throw new Error("Missing patch.");
+            }
+            if (lastRevision + 1 !== next.value.revision) {
+              throw new Error("Wrong revision received.");
+            }
+
+            mutableData = applyPatch(mutableData as Record<string, unknown>, next.value.patch);
+            valueToPublish.data = { ...mutableData } as Record<string, unknown>;
+
+            lastRevision++;
+          }
+
+          if (next.value.extensions) {
+            valueToPublish.extensions = next.value.extensions;
+          }
+          if (next.value.errors) {
+            valueToPublish.errors = next.value.errors;
+          }
+
+          await push(valueToPublish as TExecutionResult);
+          continue;
+        }
+
+        await push(next.value as TExecutionResult);
+      }
+
+      stop();
+    });
