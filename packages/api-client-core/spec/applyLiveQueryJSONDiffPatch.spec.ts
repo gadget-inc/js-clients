9230e2d2bcc6f9b4612b8e2ab10d900c3779f799
Maneet Goyal <maneetgoyal14@gmail.com>
2024-03-13 08:47:10 +0530

    refactor: add test tp check for error

--- packages/api-client-core/spec/applyLiveQueryJSONDiffPatch.spec.ts
+++ packages/api-client-core/spec/applyLiveQueryJSONDiffPatch.spec.ts
@@ -130,3 +130,31 @@ it("source.return is called for cleanup", async () => {
 
     expect(isCalled).toEqual(true);
 });
+
+it("source.return is called for cleanup if error occurred in createApplyLiveQueryPatch", async () => {
+    let isCalled = false;
+    let counter = 1;
+    const source: AsyncGenerator<Record<string, unknown>> = {
+        [Symbol.asyncIterator]() {
+            return source;
+        },
+        next: async () => {
+            // thow error when counter will be 4
+            return ({ done: false, value: { revision: counter === 4 ? 3 : 1, data: counter++ } });
+        },
+        return: async () => {
+            isCalled = true;
+            return ({ done: true, value: null });
+        },
+        async throw() {
+            throw new Error("Noop.");
+        },
+    }
+    const asyncIterableIterator = applyLiveQueryJSONDiffPatch(source);
+    try {
+        for await (const _i of asyncIterableIterator) {
+            /* empty */
+        }
+    } catch (error) { /* empty */ }
+    expect(isCalled).toEqual(true);
+});
