8ceed61bafbaa6a8c2f05647510db7fc3ac648a8
Alan Ko <34646302+alanko0511@users.noreply.github.com>
2024-07-09 13:32:35 -0400

    Introduce cypress utils for easier testing

--- packages/react/cypress/support/commands.ts
+++ packages/react/cypress/support/commands.ts
@@ -1,38 +1,69 @@
+import type { AnyClient } from "@gadgetinc/api-client-core";
 import "cypress-each";
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+import { recordIdInputField } from "../../spec/auto/support/shared.js";
+
+Cypress.Commands.add("mockModelActionMetadata", (api: AnyClient, props) => {
+  const { modelName, action, inputFields } = props;
+
+  cy.intercept(
+    {
+      method: "POST",
+      url: `${api.connection.endpoint}?operation=ModelActionMetadata`,
+    },
+    {
+      data: {
+        gadgetMeta: {
+          model: {
+            name: modelName,
+            action: {
+              ...action,
+              name: action.apiIdentifier,
+              inputFields: action.operatesWithRecordIdentity ? [recordIdInputField, ...inputFields] : inputFields,
+              __typename: "GadgetAction",
+            },
+            __typename: "GadgetModel",
+          },
+          __typename: "GadgetApplicationMeta",
+        },
+      },
+    }
+  ).as("ModelCreateActionMetadata");
+});
+
+Cypress.Commands.add("mockGetDirectUploadToken", (api: AnyClient, customToken) => {
+  const token = customToken ?? "token123";
+  cy.intercept(
+    {
+      method: "POST",
+      url: `${api.connection.endpoint}?operation=GetDirectUploadToken`,
+    },
+    {
+      data: {
+        gadgetMeta: {
+          directUploadToken: {
+            url: `https://storage.googleapis.com/gadget-storage-uploads/direct-uploads/${token}`,
+            token,
+            __typename: "DirectUploadToken",
+          },
+          __typename: "GadgetApplicationMeta",
+        },
+      },
+    }
+  ).as("getDirectUploadToken");
+});
+
+Cypress.Commands.add("mockUploadFile", (customToken) => {
+  const token = customToken ?? "token123";
+
+  cy.intercept(
+    {
+      method: "PUT",
+      url: `https://storage.googleapis.com/gadget-storage-uploads/direct-uploads/${token}`, // The URL should be the one returned by the getDirectUploadToken query
+    },
+    {} // The payload is not important for this test
+  ).as("uploadFile");
+});
+
+Cypress.Commands.add("getSubmitButton", () => {
+  return cy.get("form [type=submit][aria-hidden!=true]");
+});
