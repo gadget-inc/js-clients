dd6474f5ec7ef6e69914df0e23dccc256579a529
danroberts <danielrobertsdesign@gmail.com>
2024-07-10 09:41:33 -0400

    wip

---
+++ packages/react/spec/auto/PolarisAutoTable.stories.jsx
@@ -0,0 +1,46 @@
+import { Provider } from '../../src/GadgetProvider.tsx';
+import { PolarisAutoTable } from '../../src/auto/polaris/PolarisAutoTable.tsx';
+import { AppProvider, Page, LegacyCard } from '@shopify/polaris';
+import { testApi as api } from '../apis.ts';
+import React from 'react';
+import { PolarisAutoInput } from '../../src/auto/polaris/inputs/PolarisAutoInput.tsx';
+import { PolarisAutoSubmit } from '../../src/auto/polaris/PolarisAutoSubmit.tsx';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: 'Polaris/AutoTable',
+  component: PolarisAutoTable,
+  decorators: [
+    // 👇 Defining the decorator in the preview file applies it to all stories
+    (Story) => {
+      // 👇 Make it configurable by reading the theme value from parameters
+      return (
+        <Provider api={api}>
+          <AppProvider>
+              <LegacyCard>
+                <div style={{maxHeight: '70vh', overflow: 'auto'}}>
+                  <Story /> 
+                </div>
+              </LegacyCard>
+          </AppProvider>
+        </Provider>
+      );
+    },
+  ],
+  
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary = {
+  args: {
+    model: api.gizmo
+  },
+};
