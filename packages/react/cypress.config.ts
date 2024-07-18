3c7fd2387eb5623e148b418eb20615485ab7eeee
danroberts <danielrobertsdesign@gmail.com>
2024-07-18 16:00:17 -0400

    test(cypress): disable testing in real chrome browser

--- packages/react/cypress.config.ts
+++ packages/react/cypress.config.ts
@@ -11,15 +11,6 @@ export default defineConfig({
         },
       },
     },
-    setupNodeEvents(on, config) {
-      on("before:browser:launch", (_browser, launchOptions) => {
-        if (process.env["CI"]) {
-          // try to fix https://github.com/cypress-io/cypress/issues/29860
-          launchOptions.args.push("--disable-gpu");
-        }
-        return launchOptions;
-      });
-    },
   },
   retries: process.env["CI"] ? 2 : 0,
 });
