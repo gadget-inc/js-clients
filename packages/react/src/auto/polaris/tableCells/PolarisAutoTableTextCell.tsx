5c2e069cffc2680329f08340936944f6191a94ad
danroberts <danielrobertsdesign@gmail.com>
2024-07-26 14:39:28 -0400

    fix(autotable): enable tooltip on hover for string cells with truncated text

--- packages/react/src/auto/polaris/tableCells/PolarisAutoTableTextCell.tsx
+++ packages/react/src/auto/polaris/tableCells/PolarisAutoTableTextCell.tsx
@@ -1,13 +1,34 @@
-import { Text } from "@shopify/polaris";
-import React from "react";
+import { Tooltip } from "@shopify/polaris";
+import React, { useEffect, useRef, useState } from "react";
 
 export const PolarisAutoTableTextCell = (props: { value: any }) => {
   const { value } = props;
   const stringifiedValue = typeof value === "object" ? ("markdown" in value ? value.markdown : JSON.stringify(value)) : value;
 
-  return (
-    <Text as="span" truncate>
+  const [isOverflowed, setIsOverflow] = useState(false);
+  const textElementRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    setIsOverflow(!!textElementRef.current && textElementRef.current.scrollWidth > textElementRef.current.clientWidth);
+  }, []);
+
+  const polarisTextWithRef = (
+    <span className="Polaris-Text--root Polaris-Text--block Polaris-Text--truncate" ref={textElementRef}>
       {stringifiedValue}
-    </Text>
+    </span>
+  );
+
+  if (!isOverflowed) {
+    return polarisTextWithRef;
+  }
+
+  return (
+    <Tooltip
+      width="wide"
+      persistOnClick
+      content={<div style={{ maxHeight: "200px", overflow: "auto", pointerEvents: "auto" }}>{stringifiedValue}</div>}
+    >
+      {polarisTextWithRef}
+    </Tooltip>
   );
 };
