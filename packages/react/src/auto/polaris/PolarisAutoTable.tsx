57acc5e5013ebad8bb108646482fb841d5e657de
Harry Brundage <harry.brundage@gmail.com>
2024-08-31 15:02:40 -0400

    Fix lints from rename

--- packages/react/src/auto/polaris/PolarisAutoTable.tsx
+++ packages/react/src/auto/polaris/PolarisAutoTable.tsx
@@ -14,8 +14,8 @@ import {
 import pluralize from "pluralize";
 import type { ReactNode } from "react";
 import React, { useCallback, useMemo } from "react";
-import { useTable } from "../../useTable.js";
 import type { TableColumn, TableRow } from "../../use-table/types.js";
+import { useTable } from "../../useTable.js";
 import type { ColumnValueType, OptionsType } from "../../utils.js";
 import type { AutoTableProps } from "../AutoTable.js";
 import { AutoTableContext } from "../AutoTableContext.js";
--- packages/react/src/useTable.tsx
+++ packages/react/src/useTable.tsx
@@ -8,9 +8,9 @@ import {
 } from "@gadgetinc/api-client-core";
 import { useCallback, useMemo, useState } from "react";
 import { useModelMetadata } from "./metadata.js";
-import { useList } from "./useList.js";
 import { getTableColumns, getTableRows, getTableSelectionMap, getTableSpec } from "./use-table/helpers.js";
 import type { TableOptions, TableResult } from "./use-table/types.js";
+import { useList } from "./useList.js";
 import { deepMerge, type OptionsType, type ReadOperationOptions } from "./utils.js";
 
 const getNextDirection = (sortDirection: SortOrder | undefined) => {
