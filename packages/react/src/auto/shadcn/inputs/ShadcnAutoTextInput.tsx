85391285f3ed0a39a88c366cd61c4db41e416645
Jolaade Adewale <anthonywale64@gmail.com>
2025-01-21 19:41:45 -0500

    Remove reference in spec from src

--- packages/react/src/auto/shadcn/inputs/ShadcnAutoTextInput.tsx
+++ packages/react/src/auto/shadcn/inputs/ShadcnAutoTextInput.tsx
@@ -6,9 +6,6 @@ import { autoInput } from "../../AutoInput.js";
 import { useStringInputController } from "../../hooks/useStringInputController.js";
 import type { ShadcnElements } from "../elements.js";
 
-// should we move this to a shared location?
-import { cn } from "../../../../spec/auto/shadcn-defaults/utils.js";
-
 export const makeShadcnAutoTextInput = ({ Input, Label }: Pick<ShadcnElements, "Input" | "Label">) =>
   autoInput(
     (
@@ -36,7 +33,7 @@ export const makeShadcnAutoTextInput = ({ Input, Label }: Pick<ShadcnElements, "
               required={stringInputController.metadata.requiredArgumentForInput || false}
               {...restProps}
               placeholder={stringInputController.placeholder || label}
-              className={cn(props.className, stringInputController.errorMessage && "border-red-500")}
+              className={`${props.className ?? ""} ${stringInputController.errorMessage ? "border-red-500" : ""}`}
             />
             {suffix && <div className="">{suffix}</div>}
           </div>
