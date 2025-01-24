3c5374a859e5e8dbf696ebe2660c146bf19b1c63
Jolaade Adewale <anthonywale64@gmail.com>
2025-01-24 09:33:30 -0500

    Further optimize autoform

--- packages/react/src/auto/shadcn/ShadcnAutoForm.tsx
+++ packages/react/src/auto/shadcn/ShadcnAutoForm.tsx
@@ -15,22 +15,29 @@ import { makeSubmitResultBanner } from "./submit/ShadcnSubmitResultBanner.js";
 /**
  * Renders a form for an action on a model automatically using Shadcn
  */
-export const makeAutoForm =
-  <Elements extends ShadcnElements>({ Form, Input, Button, Alert, Skeleton, AlertTitle, AlertDescription, Label, Checkbox }: Elements) =>
-  <GivenOptions extends OptionsType, SchemaT, ActionFunc extends ActionFunction<GivenOptions, any, any, SchemaT, any>>(
+export const makeAutoForm = <Elements extends ShadcnElements>({
+  Form,
+  Input,
+  Button,
+  Alert,
+  Skeleton,
+  AlertTitle,
+  AlertDescription,
+  Label,
+  Checkbox,
+}: Elements) => {
+  const ShadcnAutoInput = makeShadcnAutoInput({ Input, Label, Button, Checkbox });
+  const ShadcnAutoSubmit = makeShadcnAutoSubmit({ Button });
+
+  return <GivenOptions extends OptionsType, SchemaT, ActionFunc extends ActionFunction<GivenOptions, any, any, SchemaT, any>>(
     props: AutoFormProps<GivenOptions, SchemaT, ActionFunc> & ComponentProps<typeof Form>
   ) => {
     const { action, findBy } = props as AutoFormProps<GivenOptions, SchemaT, ActionFunc> &
       Omit<Partial<FormProps>, "action"> & { findBy: any };
-
     validateAutoFormProps(props);
 
     // Component key to force re-render when the action or findBy changes
     const componentKey = `${action.modelApiIdentifier ?? ""}.${action.operationName}.${JSON.stringify(findBy)}`;
-
-    const ShadcnAutoInput = makeShadcnAutoInput({ Input, Label, Button, Checkbox });
-
-    const ShadcnAutoSubmit = makeShadcnAutoSubmit({ Button });
 
     return (
       <AutoFormFieldsFromChildComponentsProvider>
@@ -43,103 +50,104 @@ export const makeAutoForm =
     );
   };
 
-function ShadcnAutoFormComponent<
-  GivenOptions extends OptionsType,
-  SchemaT,
-  ActionFunc extends ActionFunction<GivenOptions, any, any, SchemaT, any>
->(
-  props: AutoFormProps<GivenOptions, SchemaT, ActionFunc> & {
-    elements: ShadcnElements;
-  } & ComponentProps<any>
-) {
-  const {
-    record: _record,
-    action,
-    findBy,
-    ...rest
-  } = props as AutoFormProps<GivenOptions, SchemaT, ActionFunc> & Omit<Partial<FormProps>, "action"> & { findBy: any };
-  const { Form, Alert, Skeleton, AlertTitle, AlertDescription, ShadcnAutoInput, ShadcnAutoSubmit, Button } = props.elements;
+  function ShadcnAutoFormComponent<
+    GivenOptions extends OptionsType,
+    SchemaT,
+    ActionFunc extends ActionFunction<GivenOptions, any, any, SchemaT, any>
+  >(
+    props: AutoFormProps<GivenOptions, SchemaT, ActionFunc> & {
+      elements: ShadcnElements;
+    } & ComponentProps<any>
+  ) {
+    const {
+      record: _record,
+      action,
+      findBy,
+      ...rest
+    } = props as AutoFormProps<GivenOptions, SchemaT, ActionFunc> & Omit<Partial<FormProps>, "action"> & { findBy: any };
+    const { Form, Alert, Skeleton, AlertTitle, AlertDescription, ShadcnAutoInput, ShadcnAutoSubmit, Button } = props.elements;
 
-  const {
-    metadata,
-    fetchingMetadata,
-    metadataError,
-    fields,
-    submit,
-    formError,
-    isSubmitting,
-    isSubmitSuccessful,
-    originalFormMethods,
-    isLoading,
-  } = useAutoForm(props);
+    const {
+      metadata,
+      fetchingMetadata,
+      metadataError,
+      fields,
+      submit,
+      formError,
+      isSubmitting,
+      isSubmitSuccessful,
+      originalFormMethods,
+      isLoading,
+    } = useAutoForm(props);
 
-  const { ShadcnSubmitSuccessfulBanner, ShadcnSubmitErrorBanner } = React.useMemo(
-    () =>
-      makeSubmitResultBanner({
-        Alert,
-        AlertTitle,
-        AlertDescription,
-        Button,
-      }),
-    []
-  );
+    const { ShadcnSubmitSuccessfulBanner, ShadcnSubmitErrorBanner } = React.useMemo(
+      () =>
+        makeSubmitResultBanner({
+          Alert,
+          AlertTitle,
+          AlertDescription,
+          Button,
+        }),
+      []
+    );
 
-  const formTitle = props.title === undefined ? humanizeCamelCase(action.operationName) : props.title;
+    const formTitle = props.title === undefined ? humanizeCamelCase(action.operationName) : props.title;
 
-  if (props.successContent && isSubmitSuccessful) {
-    return props.successContent;
-  }
+    if (props.successContent && isSubmitSuccessful) {
+      return props.successContent;
+    }
 
-  if (fetchingMetadata) {
+    if (fetchingMetadata) {
+      return (
+        <Form {...rest} onSubmit={submit}>
+          <Skeleton />
+        </Form>
+      );
+    }
+
+    const autoFormMetadataContext: AutoFormMetadataContext = {
+      findBy,
+      submit,
+      metadata,
+      submitResult: {
+        isSuccessful: isSubmitSuccessful,
+        error: formError ?? metadataError,
+        isSubmitting,
+      },
+      model: {
+        apiIdentifier: action.modelApiIdentifier,
+        namespace: action.namespace,
+      },
+      fields,
+    };
+
+    const formContent = props.children ?? (
+      <>
+        {formTitle && <h2 className="text-2xl font-bold">{formTitle}</h2>}
+        {!props.onSuccess && <ShadcnSubmitSuccessfulBanner />}
+        {!props.onFailure && <ShadcnSubmitErrorBanner />}
+        {!metadataError && (
+          <>
+            {fields.map(({ metadata }) => (
+              <ShadcnAutoInput field={metadata.apiIdentifier} key={metadata.apiIdentifier} />
+            ))}
+            <ShadcnAutoSubmit>{props.submitLabel ?? "Submit"}</ShadcnAutoSubmit>
+          </>
+        )}
+      </>
+    );
+
     return (
-      <Form {...rest} onSubmit={submit}>
-        <Skeleton />
-      </Form>
+      <AutoFormMetadataContext.Provider value={autoFormMetadataContext}>
+        <FormProvider {...originalFormMethods}>
+          {isLoading && <Skeleton />}
+          <div hidden={isLoading}>
+            <Form {...rest} onSubmit={submit}>
+              {formContent}
+            </Form>
+          </div>
+        </FormProvider>
+      </AutoFormMetadataContext.Provider>
     );
   }
-
-  const autoFormMetadataContext: AutoFormMetadataContext = {
-    findBy,
-    submit,
-    metadata,
-    submitResult: {
-      isSuccessful: isSubmitSuccessful,
-      error: formError ?? metadataError,
-      isSubmitting,
-    },
-    model: {
-      apiIdentifier: action.modelApiIdentifier,
-      namespace: action.namespace,
-    },
-    fields,
-  };
-
-  const formContent = props.children ?? (
-    <>
-      {formTitle && <h2 className="text-2xl font-bold">{formTitle}</h2>}
-      {!props.onSuccess && <ShadcnSubmitSuccessfulBanner />}
-      {!props.onFailure && <ShadcnSubmitErrorBanner />}
-      {!metadataError && (
-        <>
-          {fields.map(({ metadata }) => (
-            <ShadcnAutoInput field={metadata.apiIdentifier} key={metadata.apiIdentifier} />
-          ))}
-          <ShadcnAutoSubmit>{props.submitLabel ?? "Submit"}</ShadcnAutoSubmit>
-        </>
-      )}
-    </>
-  );
-
-  return (
-    <AutoFormMetadataContext.Provider value={autoFormMetadataContext}>
-      <FormProvider {...originalFormMethods}>
-        {isLoading && <Skeleton />}
-        <div hidden={isLoading}>
-          <Form {...rest} onSubmit={submit}>
-            {formContent}
-          </Form>
-        </div>
-      </FormProvider>
-    </AutoFormMetadataContext.Provider>
-  );
-}
+};
