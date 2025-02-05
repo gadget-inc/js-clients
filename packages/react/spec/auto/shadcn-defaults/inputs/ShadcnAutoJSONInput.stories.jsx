3b0f6328b3883a400cdca3bc176e23003b4e21d6
MillanWangGadget <millan@gadget.dev>
2025-02-05 18:02:22 -0500

    fix shadcn storybook imports

--- packages/react/spec/auto/shadcn-defaults/inputs/ShadcnAutoJSONInput.stories.jsx
+++ packages/react/spec/auto/shadcn-defaults/inputs/ShadcnAutoJSONInput.stories.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Provider } from "../../../../src/GadgetProvider.tsx";
 import { makeShadcnAutoJSONInput } from "../../../../src/auto/shadcn/inputs/ShadcnAutoJSONInput.tsx";
-import { makeAutocomponents } from "../../../../src/auto/shadcn/unreleasedIndex.js";
+import { makeAutocomponents } from "../../../../src/auto/shadcn/unreleasedIndex.ts";
 import { FormProvider, useForm } from "../../../../src/useActionForm.ts";
 import { testApi as api } from "../../../apis.ts";
-import { elements } from "../index";
+import { elements } from "../index.tsx";
+
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 
 const ShadcnAutoForm = makeAutocomponents(elements).AutoForm;
