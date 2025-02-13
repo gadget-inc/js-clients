bfd1d2b05fa5256e7090e37b3c379223d06062ce
Jolaade Adewale <anthonywale64@gmail.com>
2025-02-13 10:11:15 -0500

    Add Button props to AutoSubmit

--- packages/react/src/auto/shadcn/submit/ShadcnAutoSubmit.tsx
+++ packages/react/src/auto/shadcn/submit/ShadcnAutoSubmit.tsx
@@ -1,12 +1,10 @@
-import type { ButtonHTMLAttributes, ReactNode } from "react";
+import type { ReactNode } from "react";
 import React from "react";
 import { useAutoFormMetadata } from "../../AutoFormContext.js";
-import type { ShadcnElements } from "../elements.js";
+import type { ButtonProps, ShadcnElements } from "../elements.js";
 
 export const makeShadcnAutoSubmit = ({ Button }: Pick<ShadcnElements, "Button">) => {
-  function ShadcnAutoSubmit(
-    props: { children?: ReactNode; isSubmitting?: boolean; className?: string } & Partial<ButtonHTMLAttributes<HTMLButtonElement>>
-  ) {
+  function ShadcnAutoSubmit(props: { children?: ReactNode; isSubmitting?: boolean; className?: string } & ButtonProps) {
     const { submitResult } = useAutoFormMetadata();
     const isSubmitting = props.isSubmitting ?? submitResult.isSubmitting;
 
