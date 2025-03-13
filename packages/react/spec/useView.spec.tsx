d6d9f08b1a47bfeea156b671f899318a5c3e4907
Harry Brundage <harry.brundage@gmail.com>
2025-03-12 21:56:31 -0400

    Add useView hook for consuming computed views
    [no-changelog-required]

---
+++ packages/react/spec/useView.spec.tsx
@@ -0,0 +1,243 @@
+import { renderHook } from "@testing-library/react";
+import type { IsExact } from "conditional-type-checks";
+import { assert } from "conditional-type-checks";
+import { useView } from "../src/useView.js";
+import type { ErrorWrapper } from "../src/utils.js";
+import { testApi } from "./apis.js";
+import { MockClientWrapper, createMockUrqlClient, mockUrqlClient } from "./testWrappers.js";
+
+describe("useView", () => {
+  // all these functions are typechecked but never run to avoid actually making API calls
+  const _TestUseViewNoVariablesReturnsTypedData = () => {
+    const [{ data, fetching, error }, refresh] = useView(testApi.totalInStock);
+
+    assert<IsExact<typeof fetching, boolean>>(true);
+    assert<IsExact<typeof data, undefined | { totalInStock: number | null }>>(true);
+    assert<IsExact<typeof error, ErrorWrapper | undefined>>(true);
+
+    if (data) {
+      data.totalInStock;
+    }
+
+    refresh();
+  };
+
+  const _TestUseViewVariablesReturnsTypedData = () => {
+    const [{ data, fetching, error }, refresh] = useView(testApi.echo, { value: "test" });
+
+    assert<IsExact<typeof fetching, boolean>>(true);
+    assert<IsExact<typeof data, undefined | { value: string | null }>>(true);
+    assert<IsExact<typeof error, ErrorWrapper | undefined>>(true);
+
+    if (data) {
+      data.value;
+    }
+
+    refresh();
+  };
+
+  const _TestUseViewOptions = () => {
+    const [{ data, fetching, error }, refresh] = useView(testApi.echo, { value: "test" }, { pause: true });
+
+    assert<IsExact<typeof fetching, boolean>>(true);
+    assert<IsExact<typeof data, undefined | { value: string | null }>>(true);
+    assert<IsExact<typeof error, ErrorWrapper | undefined>>(true);
+
+    if (data) {
+      data.value;
+    }
+
+    refresh();
+  };
+
+  const _TestUseNamespacedView = () => {
+    const [{ data }] = useView(testApi.game.echo, { value: 123 });
+
+    assert<IsExact<typeof data, undefined | { value: number | null }>>(true);
+
+    if (data) {
+      data.value;
+    }
+  };
+
+  test("can fetch a view with no variables", async () => {
+    let query: string | undefined;
+    const client = createMockUrqlClient({
+      queryAssertions: (request) => {
+        query = request.query.loc?.source.body;
+      },
+    });
+
+    const { result } = renderHook(() => useView(testApi.totalInStock), { wrapper: MockClientWrapper(testApi, client) });
+
+    expect(result.current[0].data).toBeFalsy();
+    expect(result.current[0].fetching).toBe(true);
+    expect(result.current[0].error).toBeFalsy();
+
+    expect(client.executeQuery).toHaveBeenCalledTimes(1);
+
+    expect(query).toMatchInlineSnapshot(`
+      "query totalInStock {
+        totalInStock
+      }"
+    `);
+
+    client.executeQuery.pushResponse("totalInStock", {
+      data: {
+        totalInStock: 100,
+      },
+      stale: false,
+      hasNext: false,
+    });
+
+    expect(result.current[0].data!).toEqual(100);
+    expect(result.current[0].fetching).toBe(false);
+    expect(result.current[0].error).toBeFalsy();
+  });
+
+  test("can fetch a view with variables", async () => {
+    let query: string | undefined;
+    const client = createMockUrqlClient({
+      queryAssertions: (request) => {
+        query = request.query.loc?.source.body;
+      },
+    });
+
+    const { result } = renderHook(() => useView(testApi.echo, { value: "test" }), { wrapper: MockClientWrapper(testApi, client) });
+
+    expect(result.current[0].data).toBeFalsy();
+    expect(result.current[0].fetching).toBe(true);
+    expect(result.current[0].error).toBeFalsy();
+
+    expect(client.executeQuery).toHaveBeenCalledTimes(1);
+
+    expect(query).toMatchInlineSnapshot(`
+      "query echo($value: undefined) {
+        echo(value: $value)
+      }"
+    `);
+
+    client.executeQuery.pushResponse("echo", {
+      data: {
+        echo: {
+          value: "test",
+        },
+      },
+      stale: false,
+      hasNext: false,
+    });
+
+    expect(result.current[0].data!.value).toEqual("test");
+    expect(result.current[0].fetching).toBe(false);
+    expect(result.current[0].error).toBeFalsy();
+  });
+
+  test("can find namespaced view", async () => {
+    let query: string | undefined;
+    const client = createMockUrqlClient({
+      queryAssertions: (request) => {
+        query = request.query.loc?.source.body;
+      },
+    });
+
+    const { result } = renderHook(() => useView(testApi.game.echo, { value: 123 }), { wrapper: MockClientWrapper(testApi, client) });
+
+    expect(result.current[0].data).toBeFalsy();
+    expect(result.current[0].fetching).toBe(true);
+    expect(result.current[0].error).toBeFalsy();
+
+    expect(client.executeQuery).toHaveBeenCalledTimes(1);
+
+    expect(query).toMatchInlineSnapshot(`
+      "query echo($value: undefined) {
+        game {
+          echo(value: $value)
+        }
+      }"
+    `);
+
+    client.executeQuery.pushResponse("echo", {
+      data: {
+        game: {
+          echo: {
+            value: 123,
+          },
+        },
+      },
+      stale: false,
+      hasNext: false,
+    });
+
+    expect(result.current[0].data!.value).toEqual(123);
+    expect(result.current[0].fetching).toBe(false);
+    expect(result.current[0].error).toBeFalsy();
+  });
+
+  test("returns the same data object on rerender if nothing changes about the result", async () => {
+    const { result, rerender } = renderHook(() => useView(testApi.echo, { value: "test" }), { wrapper: MockClientWrapper(testApi) });
+
+    expect(result.current[0].data).toBeFalsy();
+    expect(result.current[0].fetching).toBe(true);
+    expect(result.current[0].error).toBeFalsy();
+
+    expect(mockUrqlClient.executeQuery).toHaveBeenCalledTimes(1);
+
+    mockUrqlClient.executeQuery.pushResponse("echo", {
+      data: {
+        echo: {
+          value: "test",
+        },
+      },
+      stale: false,
+      hasNext: false,
+    });
+
+    const beforeObject = result.current[0];
+
+    rerender();
+
+    expect(result.current[0]).toBe(beforeObject);
+  });
+
+  test("suspends when loading data", async () => {
+    const { result, rerender } = renderHook(() => useView(testApi.echo, { value: "test" }, { suspense: true }), {
+      wrapper: MockClientWrapper(testApi),
+    });
+
+    // first render never completes as the component suspends
+    expect(result.current).toBeFalsy();
+    expect(mockUrqlClient.executeQuery).toHaveBeenCalledTimes(1);
+
+    mockUrqlClient.executeQuery.pushResponse("echo", {
+      data: {
+        echo: {
+          value: "test",
+        },
+      },
+      stale: false,
+      hasNext: false,
+    });
+
+    // rerender as react would do when the suspense promise resolves
+    rerender();
+    expect(result.current).toBeTruthy();
+    expect(result.current[0].data!.value).toEqual("test");
+    expect(result.current[0].error).toBeFalsy();
+
+    const beforeObject = result.current[0];
+    rerender();
+    expect(result.current[0]).toBe(beforeObject);
+  });
+
+  test("doesn't issue a request if paused", async () => {
+    const { result } = renderHook(() => useView(testApi.echo, { value: "test" }, { pause: true }), {
+      wrapper: MockClientWrapper(testApi),
+    });
+
+    expect(result.current[0].data).toBeFalsy();
+    expect(result.current[0].fetching).toBe(false);
+    expect(result.current[0].error).toBeFalsy();
+
+    expect(mockUrqlClient.executeQuery).toHaveBeenCalledTimes(0);
+  });
+});
