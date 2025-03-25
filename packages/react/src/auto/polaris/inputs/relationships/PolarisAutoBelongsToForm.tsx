af22f4437f48253708cdcea56cf6888f03b3416a
Matin <matin.damyar@gmail.com>
2025-03-25 18:01:05 -0400

    Comment change for AutoComponents

--- packages/react/src/auto/polaris/inputs/relationships/PolarisAutoBelongsToForm.tsx
+++ packages/react/src/auto/polaris/inputs/relationships/PolarisAutoBelongsToForm.tsx
@@ -9,25 +9,25 @@ import { SearchableSingleRelatedModelRecordSelector } from "./SearchableSingleRe
 import { renderOptionLabel } from "./utils.js";
 
 /**
- * A belongsTo field form component for use within <AutoForm></AutoForm> components
+ * A belongsTo field form used within AutoForm.
  * This component allows related model records to be created or updated from the current model.
  * Fields on the related model record are controlled with the nested <AutoInput/> child components.
  *
  * @example
- * ```tsx
+ * ```
  * <AutoForm action={api.modelA.create}>
  *   <AutoBelongsToForm field="parentModel" >
  *     <AutoInput field="parentModelField" />
  *   </AutoBelongsToForm>
  * </AutoForm>
  * ```
- * @param props.field - The belongsTo field API identifier
- * @param props.label - The label of the belongTo form component
- * @param props.children - The child components to be rendered within the form. This is intended to be used with <AutoInput/>.
- * @param props.optionLabel - Controls how records on the related model are displayed as options in the relationship field input component.
- *                            When using a string, the string will indicate the field on the related model record to be displayed as the option label.
- *                            When using a function, the function will be called with the record to return a ReactNode to be displayed as the option label
- * @returns The belongsTo field form component
+ * @param props.field - The belongsTo field API identifier.
+ * @param props.label - Label of the belongTo form component
+ * @param props.children - Child components to render within the form, typically <AutoInput/> components.
+ * @param props.optionLabel - Controls the display of related model records in the input component.
+ *                           Use a string to specify which field to display, or
+ *                           Use a function that returns a ReactNode for custom display
+ * @returns The AutoBelongsToForm component.
  */
 export const PolarisAutoBelongsToForm = autoRelationshipForm((props: AutoRelationshipFormProps) => {
   const belongsToForm = useBelongsToForm(props);
