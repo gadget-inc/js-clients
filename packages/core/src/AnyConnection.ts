e716377a061ebac9a039c76499189b8c2f508ebb
infiton <kbt.tate@gmail.com>
2025-10-04 11:30:59 -0400

    adds the extracted @gadgetinc/core package

---
+++ packages/core/src/AnyConnection.ts
@@ -0,0 +1,56 @@
+import type { Client, ClientOptions } from "@urql/core";
+import type { ClientOptions as SubscriptionClientOptions, createClient as createSubscriptionClient } from "graphql-ws";
+import type { AuthenticationModeOptions, Exchanges } from "./ClientOptions.js";
+import { GadgetTransaction } from "./GadgetTransaction.js";
+
+export interface GadgetSubscriptionClientOptions extends Partial<SubscriptionClientOptions> {
+  urlParams?: Record<string, string | null | undefined>;
+  connectionAttempts?: number;
+  connectionGlobalTimeoutMs?: number;
+}
+
+/**
+ * Represents the current strategy for authenticating with the Gadget platform.
+ * For individual users in web browsers, we authenticate using a session token stored client side, like a cookie, but with cross domain support.
+ * For server to server communication, or traceable access from the browser, we use pre shared secrets called API Keys
+ * And when within the Gadget platform itself, we use a private secret token called an Internal Auth Token. Internal Auth Tokens are managed by Gadget and should never be used by external developers.
+ **/
+export enum AuthenticationMode {
+  BrowserSession = "browser-session",
+  APIKey = "api-key",
+  Internal = "internal",
+  InternalAuthToken = "internal-auth-token",
+  Anonymous = "anonymous",
+  Custom = "custom",
+}
+
+export interface GadgetConnectionOptions {
+  endpoint: string;
+  authenticationMode?: AuthenticationModeOptions;
+  websocketsEndpoint?: string;
+  subscriptionClientOptions?: GadgetSubscriptionClientOptions;
+  websocketImplementation?: typeof globalThis.WebSocket;
+  fetchImplementation?: typeof globalThis.fetch;
+  environment?: string;
+  requestPolicy?: ClientOptions["requestPolicy"];
+  applicationId?: string;
+  baseRouteURL?: string;
+  exchanges?: Exchanges;
+  createSubscriptionClient?: typeof createSubscriptionClient;
+}
+
+export type TransactionRun<T> = (transaction: GadgetTransaction) => Promise<T>;
+
+export interface AnyConnection {
+  endpoint: string;
+  authenticationMode: AuthenticationMode;
+  createSubscriptionClient: typeof createSubscriptionClient;
+  options: GadgetConnectionOptions;
+  get currentClient(): Client;
+  transaction: {
+    <T>(options: GadgetSubscriptionClientOptions, run: TransactionRun<T>): Promise<T>;
+    <T>(run: TransactionRun<T>): Promise<T>;
+  };
+  close(): void;
+  fetch: (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>;
+}
---
+++ packages/core/src/GadgetTransaction.ts
@@ -0,0 +1,52 @@
+import type { Client } from "@urql/core";
+import type { Client as SubscriptionClient } from "graphql-ws";
+import { assertOperationSuccess } from "./support.js";
+
+/** Represents the error thrown when a transaction is explicity rolled back, sometimes due to another inner error */
+export class TransactionRolledBack extends Error {}
+
+/** Represents an open transaction against the Gadget API */
+export class GadgetTransaction {
+  open = false;
+  constructor(readonly client: Client, readonly subscriptionClient: SubscriptionClient) {}
+
+  /** Shut down this transaction by closing the connection to the backend. */
+  close() {
+    if (this.open) {
+      void this.rollback().catch(() => null);
+    }
+    void this.subscriptionClient.dispose();
+  }
+
+  /** Explicitly roll back this transaction, preventing any of the changes made during it from being committed. */
+  async rollback() {
+    assertOperationSuccess(await this.client.mutation(`mutation RollbackTransaction { internal { rollbackTransaction }}`, {}).toPromise(), [
+      "internal",
+      "rollbackTransaction",
+    ]);
+    this.open = false;
+    throw new TransactionRolledBack("Transaction rolled back.");
+  }
+
+  /**
+   * @private
+   */
+  async start() {
+    assertOperationSuccess(await this.client.mutation(`mutation StartTransaction { internal { startTransaction }}`, {}).toPromise(), [
+      "internal",
+      "startTransaction",
+    ]);
+    this.open = true;
+  }
+
+  /**
+   * @private
+   */
+  async commit() {
+    assertOperationSuccess(await this.client.mutation(`mutation CommitTransaction { internal { commitTransaction }}`, {}).toPromise(), [
+      "internal",
+      "commitTransaction",
+    ]);
+    this.open = false;
+  }
+}
